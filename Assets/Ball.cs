c5d85b45d3099de5ffd57aea72e9ac10302940a1
Cosmin Clucerescu <cossmc93@gmail.com>
2019-09-17 20:23:41 +0300

    Added basic game

---
+++ Assets/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5f;
+    void Start()
+    {
+        float dx = Random.Range(0, 2) > 0 ? -1: 1 ;
+        float dy = Random.Range(0, 2) > 0 ? -1 : 1;
+        GetComponent<Rigidbody>().velocity = new Vector3(speed * dx, speed * dy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isPlayer1 = false;
+    public float speed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        var input = "Vertical";
+        if (!isPlayer1)
+            input = "Vertical2";
+        GetComponent<Rigidbody>().velocity = new Vector3(0f, Input.GetAxis(input) * speed*100 * Time.deltaTime, 0f);
+    }
+}
